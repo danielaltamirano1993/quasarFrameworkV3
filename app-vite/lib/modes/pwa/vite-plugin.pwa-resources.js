d2075690b794ebc932ab22235bfffbe6e67fae96
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-05-31 11:46:52 -0500

    Create vite-plugin.pwa-resources.js

---
+++ app-vite/lib/modes/pwa/vite-plugin.pwa-resources.js
@@ -0,0 +1,53 @@
+
+const { static: serveStatic } = require('express')
+
+const appPaths = require('../../app-paths.js')
+const { createHeadTags } = require('./utils.js')
+
+module.exports.quasarVitePluginPwaResources = function quasarVitePluginPwaResources (quasarConf) {
+  let pwaManifest = null
+  let headTags
+  let manifestContent
+
+  function updateCache () {
+    if (quasarConf.htmlVariables.pwaManifest === pwaManifest) {
+      return
+    }
+
+    pwaManifest = quasarConf.htmlVariables.pwaManifest
+    headTags = createHeadTags(quasarConf)
+    manifestContent = JSON.stringify(pwaManifest)
+  }
+
+  return {
+    name: 'quasar:pwa-resources',
+    enforce: 'pre',
+
+    transformIndexHtml: {
+      transform: html => {
+        updateCache()
+        return html.replace(
+          /(<\/head>)/i,
+          (_, tag) => `${ headTags }${ tag }`
+        )
+      }
+    },
+
+    // runs for dev only to serve manifest and service-worker
+    configureServer (server) {
+      server.middlewares.use(
+        `${ quasarConf.build.publicPath }${ quasarConf.pwa.manifestFilename }`,
+        (_, res) => {
+          updateCache()
+          res.setHeader('Content-Type', 'application/json')
+          res.end(manifestContent)
+        }
+      )
+
+      server.middlewares.use(
+        quasarConf.build.publicPath,
+        serveStatic(appPaths.resolve.app('.quasar/pwa'), { maxAge: 0 })
+      )
+    }
+  }
+}
