8f204b728ac33c9225e769b8a133683be1019041
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-07-30 13:42:45 -0500

    v9

---
+++ docs/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { useMeta } from 'quasar'
+
+import getMeta from 'assets/get-meta.js'
+
+useMeta({
+  title: 'Quasar Framework',
+  titleTemplate: title => `${title} | Quasar Framework`,
+
+  meta: getMeta(
+    'Quasar Framework - Build high-performance VueJS user interfaces in record time',
+    'Developer-oriented, front-end framework with VueJS components for best-in-class high-performance, responsive websites, PWA, SSR, Mobile and Desktop apps, all from the same codebase. Sensible people choose Vue. Productive people choose Quasar. Be both.'
+  )
+})
+</script>
