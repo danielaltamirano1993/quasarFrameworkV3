dd887c321c05870be65981eceb5cba406c661457
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-07 15:46:18 -0500

    g15

---
+++ ui/dev/quasar.config.js
@@ -0,0 +1,111 @@
+const { join } = require('path')
+const { mergeConfig } = require('vite')
+
+module.exports = function (ctx) {
+  return {
+    eslint: {
+      fix: true,
+      warnings: true,
+      errors: true
+    },
+
+    boot: [
+      ctx.mode.ssr ? { path: 'ssr-client', server: false } : ''
+    ],
+
+    css: [],
+
+    extras: [
+      'ionicons-v4',
+      'mdi-v6',
+      'fontawesome-v6',
+      'eva-icons',
+      'themify',
+      'line-awesome',
+      'bootstrap-icons',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+
+      'roboto-font',
+      'material-symbols-outlined',
+      'material-symbols-rounded',
+      'material-symbols-sharp',
+      'material-icons-outlined',
+      'material-icons-round',
+      'material-icons-sharp',
+      'material-icons'
+    ],
+
+    framework: {
+      // iconSet: 'svg-mdi-v6',
+      // config: { ripple: { early: true } },
+      // config: {
+      //   globalNodes: {
+      //     class: 'mimi'
+      //   }
+      // },
+
+      plugins: [
+        'AddressbarColor',
+        'AppFullscreen',
+        'AppVisibility',
+        'BottomSheet',
+        'Cookies',
+        'Dark',
+        'Dialog',
+        'Loading',
+        'LoadingBar',
+        'LocalStorage',
+        'Meta',
+        'Notify',
+        'Platform',
+        'Screen',
+        'SessionStorage'
+      ]
+    },
+
+    build: {
+      vueRouterMode: 'history',
+
+      // needed otherwise we need to compile Quasar UI
+      // on each source file change:
+      devQuasarTreeshaking: true,
+
+      alias: {
+        'quasar/dist/quasar.sass': join(__dirname, '../src/css/index.sass'),
+        'quasar/icon-set': join(__dirname, '../icon-set'),
+        'quasar/lang': join(__dirname, '../lang'),
+        'quasar/src': join(__dirname, '../src')
+      },
+
+      extendViteConf (viteConf, { isServer }) {
+        viteConf.server = mergeConfig(viteConf.server, {
+          fs: {
+            allow: [
+              // for quasar package (ui folder) and related deps
+              '..',
+              // due to workspace hoisting, some deps might come from the root node_modules
+              '../..'
+            ]
+          }
+        })
+
+        if (isServer) {
+          viteConf.resolve.alias.quasar = join(__dirname, '../src/index.ssr.js')
+        }
+      }
+    },
+
+    devServer: {
+      https: false,
+      open: {
+        app: { name: 'google chrome' }
+      }
+    },
+
+    ssr: {
+      middlewares: [
+        'render' // keep this as last one
+      ]
+    }
+  }
+}
---
+++ ui/dev/script.dev.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const moduleAlias = require('module-alias')
+
+moduleAlias.addAlias('quasar', path.join(__dirname, '..'))
+
+// Ensure cwd is set to ui/dev
+process.chdir(__dirname)
+
+require('@quasar/app-vite/lib/cmd/dev')
