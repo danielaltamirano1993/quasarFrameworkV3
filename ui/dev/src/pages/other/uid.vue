092fec06cc692241547eb4ff16c2217fd40e13c1
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-08 14:17:31 -0500

    h6

---
+++ ui/dev/src/pages/other/uid.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-layout-padding">
+    <pre>{{ uidResult }}</pre>
+  </div>
+</template>
+
+<script>
+import { uid } from 'quasar'
+
+export default {
+  setup () {
+    const uids = []
+    for (let i = 0; i < 100; i++) {
+      uids.push(uid())
+    }
+    return { uidResult: JSON.stringify(uids, null, 2) }
+  }
+}
+</script>
